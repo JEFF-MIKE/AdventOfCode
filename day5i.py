25cbb1ccceb2827f6e714a8df1b0a8ad6ca05ae4
Merited <michaeljefferies08@gmail.com>
2017-12-07 17:17:01 +0000

    moooarrrrr muahahhahaha

--- day5i.py
+++ day5i.py
@@ -17,4 +17,28 @@ def day5i():
         if indexer >= len(lst):
             break
     return (indexer,counter)
-print(day5i())         
+#print(day5i()) 
+
+def day5ii():
+    counter = 0
+    lst = []
+    fileR = open("input5.txt","r")
+    for line in fileR:
+        line = line.strip()
+        lst.append(int(line))
+    # array done
+    print(lst)
+    indexer = 0
+    while indexer < len(lst):
+        print(indexer)
+        prev = indexer
+        indexer += lst[prev]
+        if lst[prev] >= 3:
+            lst[prev] -= 1
+        else:
+            lst[prev] += 1
+        counter += 1
+        if indexer >= len(lst):
+            break
+    return (indexer,counter)
+print(day5ii())        
