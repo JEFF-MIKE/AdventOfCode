be36d82bccfa44c1ea49c9c34e458cbcbf1c2bc1
Merited <michaeljefferies08@gmail.com>
2017-12-07 16:24:22 +0000

    day4 complete

--- day4i.py
+++ day4i.py
@@ -15,4 +15,29 @@ def day4i():
             total += 1
     return total
 
-print(day4i())
+#print(day4i())
+
+def day4ii():
+    fileR = open("input4.txt","r")
+    total = 0
+    for line in fileR:
+        counter = True
+        splitter = line.split(" ")
+        splitter[-1] = splitter[-1].strip()
+        print(splitter)
+        for i in range(len(splitter)):
+            for j in range(i+1,len(splitter)):
+                part1 = [x for x in splitter[i]]
+                part2 = [_ for _ in splitter[j]]
+                part1.sort()
+                part2.sort()
+                if part1 == part2:
+                    counter = False               
+        if counter:
+            total += 1
+    return total
+
+print(day4ii())
+                    
+
+                    
