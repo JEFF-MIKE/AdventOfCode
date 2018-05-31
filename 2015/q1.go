c781d7b4baed0531cb8985bdcce3ae68c1257abf
MJ <michaeljefferies08@gmail.com>
2018-06-01 00:32:04 +0100

    Completed Question 1 of 2015

--- 2015/q1.go
+++ 2015/q1.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 )
-
+// using io/ioutil as opposed to bufio as I want to read
+// entire file as a single string
 func q1a()(){
 	// opens file
 	b, err := ioutil.ReadFile("q1.txt")
@@ -24,7 +25,32 @@ func q1a()(){
 	}
 	fmt.Println("The answer is:",counter)
 }
+func q1b()(){
+	// opens file
+	b, err := ioutil.ReadFile("q1.txt")
+	if err != nil{
+		fmt.Print(err)
+	}
+	x := string(b)
+	position := 0
+	counter := 0
+	for _,char := range x {
+		if string(char) == "("{
+			counter++
+		} else if string(char) == ")"{
+			counter--
+		} else {
+			continue // just ignore this
+		}
+		position++
+		if counter == -1{
+			break
+		}
+	}
+	fmt.Println("The position is:",position)
+}
 func main()(){
 	q1a()
+	q1b()
 }
 	
