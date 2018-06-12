94e52d7510f24f6a94976edcf9758865761ae7d2
MJ <michaeljefferies08@gmail.com>
2018-06-12 21:49:25 +0100

    day 6 part 1 completed

--- 2015/d6.go
+++ 2015/d6.go
@@ -17,7 +17,7 @@ func q6a(){
 		fmt.Println(err)
 	}
 	defer f.Close()
-	//var input [1000][1000]int // filled
+	var input [1000][1000]int // filled
 	var lines []string
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
@@ -33,26 +33,55 @@ func q6a(){
 			// and the lights that were turned on
 			//fmt.Println((strconv.Atoi(string(lines[i][1]))))
 			x1,_ := strconv.Atoi(n[1])
-			x2,_ := strconv.Atoi(n[2])
-			y1,_ := strconv.Atoi(n[4])
+			x2,_ := strconv.Atoi(n[4])
+			y1,_ := strconv.Atoi(n[2])
 			y2,_ := strconv.Atoi(n[5])
 			xstart := x1
 			ystart := y1
-			for x2 >= xstart {
-				for y2 >= ystart {
-					fmt.Println(y1)
-					y1++
+			for y2 >= ystart {
+				for x2 >= xstart {
+					if input[ystart][xstart] == 1{
+						input[ystart][xstart] = 0 
+					} else {
+						input[ystart][xstart] = 1
+					}
+					xstart++
 				}
-				fmt.Println(x1)
-				x1++
+				ystart++
+				xstart = x1
 			}
-			fmt.Println(x1,x2,y1,y2)
-		} else if n[1] == "on" {
-			// turn on the lights
 		} else {
-			// turn off the lights
+			// either turn off the lights or turn them on
+			x1,_ := strconv.Atoi(n[2])
+			x2,_ := strconv.Atoi(n[5])
+			y1,_ := strconv.Atoi(n[3])
+			y2,_ := strconv.Atoi(n[6])
+			xstart := x1
+			ystart := y1
+			num := 0
+			if n[1] == "on"{
+				num = 1
+			} else {
+				num = 0
+			}
+			for y2 >= ystart{
+				for x2 >= xstart{
+					input[ystart][xstart] = num
+					xstart++
+				}
+				ystart++
+				xstart = x1
+			}
 		}
 	}
+	total := 0 
+	for p := 0;p<len(input);p++{
+		for q := 0;q<len(input[p]);q++{
+			total += input[p][q]
+		}
+	}
+	fmt.Println(total)
+	//fmt.Println(input)
 }
 
 func main(){
