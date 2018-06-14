d65f060f893e16b50f2339e9905717422fc975d9
MJ <michaeljefferies08@gmail.com>
2018-06-14 23:17:51 +0100

    did some progress on day 7, bit manipulation

--- 2015/d7.go
+++ 2015/d7.go
@@ -8,7 +8,7 @@ import (
 )
 
 func q7a(){
-	f,err := os.Open("q7.txt")
+	f,err := os.Open("d7.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -16,13 +16,19 @@ func q7a(){
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 	var lines []string // empty array to hold our input
+	//var m map[string]string
 	for scanner.Scan(){
 		lines = append(lines,scanner.Text())	
 	}
 	// lines holds our input
 	for i := range lines{
-		m := strings.Split(lines[i]," ")
-		fmt.Println(m)
+		x := strings.Split(lines[i]," ")
+		if len(x) == 3 {
+			fmt.Println(x)
+		} else if (x[0] == "b") || x[2] == "b"{
+			fmt.Println(x)
+		}
+		//fmt.Println(x,len(x))
 	}
 }
 
