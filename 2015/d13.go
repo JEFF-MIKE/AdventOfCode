ee8dd74614bc2f2cb9cece70d2163528fdd77f74
MJ <michaeljefferies08@gmail.com>
2018-06-21 23:40:46 +0100

    Made more progress on day 13 challenge

--- 2015/d13.go
+++ 2015/d13.go
@@ -7,13 +7,12 @@ import (
 	"strings"
 	"strconv"
 )
-
 func d13a(){
 	var lines []string // input
 	//var p []map[string]map[string]int // holds maps with key:value pairs strings:ints
 	var m = make(map[string]map[string]int) // key is name, value is x
-	var marked = make(map[string]int)
-	f,err := os.Open("d13.txt")
+	var taken = make(map[string]map[string]int)	
+	f,err := os.Open("d13a.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -38,13 +37,42 @@ func d13a(){
 			number *= -1
 			m[n[0]][recipient] = number
 		}
-	}
-	for key,_ := range m {
-		marked[key] = 0
+		if m[recipient][n[0]] != 0 {
+			// adjust the dictionary
+			adjusted := m[n[0]][recipient] + m[recipient][n[0]]
+			m[n[0]][recipient] = adjusted
+			m[recipient][n[0]] = adjusted
 		}
-	
+	}
+	for key,_ := range m{
+		// fill up taken with dictionaries.
+		// if there are more than two in the nested dictionary,
+		// we know an adjustment has to be made
+		var z =  make(map[string]int)
+		taken[key] = z
+	}
+	total := 0
+	for key1,value1 := range m{
+		// max2 is second highest, max1 is highest
+		ref1,ref2 := "",""
+		max1,max2 := 0,0
+		for key2,_ := range value1{
+			if m[key1][key2] > max2 {
+				max2 = m[key1][key2]
+				ref2 = key2 
+				if max2 > max1{
+					max2,max1 = max1,max2
+					ref2,ref1 = ref1,ref2
+				}
+			}
+		taken[key1][ref1] = max1
+		taken[key1][ref2] = max2
+		total += (max1 + max2)
+		}
+	}
 	fmt.Println(m)
-	fmt.Println(marked)
+	fmt.Println(taken)
+	fmt.Println(total)
 	
 }
 
