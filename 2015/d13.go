44df33affc27345492909457be05abfffe66bd93
MJ <michaeljefferies08@gmail.com>
2018-06-23 17:53:04 +0100

    Banana:struct

--- 2015/d13.go
+++ 2015/d13.go
@@ -7,6 +7,132 @@ import (
 	"strings"
 	"strconv"
 )
+// decided to completely rework the code as
+// it was just not initially working out at all
+// so OOP is here to save the day.
+// previous work down below
+
+type person struct{
+	name string // hold their name for removal
+	favourablePerson map[string]int
+}
+func (p person) addPartner(matchString string,matchInt int)(){
+	p.favourablePerson[matchString] = matchInt
+}
+func (p person) getMax()(string,int){
+	maxPerson := ""
+	maxInt := 0
+	for key,value := range p.favourablePerson{
+		if value > maxInt{
+			// get the max first, then
+			maxInt = value
+			maxPerson = key
+		}
+	}
+	return maxPerson,maxInt
+}
+
+func (p person) check()(bool){
+	if len(p.favourablePerson) > 6{
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p person) removeMax(){
+	x,_ := p.getMax()
+	// remove the max once we use it
+	delete(p.favourablePerson,x)
+}
+
+type table struct{
+	// an array of people
+	seats []person
+	totalCount int
+}
+
+func (t *table) addPerson(k person)([]person){
+	t.seats = append(t.seats,k)
+	return t.seats
+}
+func (t *table) giveMax()(string,int){
+	// iterates through ALL the people to see who
+	// has the highest value
+	maxInt := 0
+	maxName := ""
+	// we will use index variable for removing from the
+	// table list
+	index := 0 
+	for k := range t.seats {
+		x,y := t.seats[k].getMax()
+		if y > maxInt{
+			maxInt = y
+			maxName = x
+			index = k
+		}
+	}
+	t.totalCount += maxInt
+	t.seats[index].removeMax()
+	t.checkPerson(index)
+	return maxName,maxInt
+}
+func (t table) checkPerson(i int)(){
+	// it only has six people
+	if t.seats[i].check() == false{
+		t.seats[i],t.seats[(len(t.seats) - 1)] = t.seats[(len(t.seats)-1)],t.seats[i]
+		t.seats = t.seats[:(len(t.seats)-1)]
+	} else {
+		
+	}
+}
+
+func d13a(){
+	var a *person
+	var x []person
+	seating := table{seats: x,totalCount: 0}
+	// when personCounter goes to 7, we reset it
+	personCounter := 0
+	listIterator := 0
+	var lines []string // input
+	f,err := os.Open("d13.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	for (scanner.Scan()){
+		lines = append(lines,scanner.Text())
+	}
+	for i := range lines{
+		n := strings.Split(lines[i]," ")
+		if personCounter == 0{
+			var o = make(map[string]int)
+			p := person{name:n[0],favourablePerson: o}
+			a = &p
+			seating.addPerson(p)
+		}
+		number,_ := strconv.Atoi(n[3])
+		if n[2] == "lose"{
+			number = number *-1
+		}
+		a.addPartner(strings.Trim(n[10],"."),number)
+		personCounter++
+		if personCounter == 7 {
+			personCounter = 0
+			listIterator++
+		}
+	}
+	for true{
+		
+	}
+}
+
+func main(){
+	d13a()
+}
+/*
 func d13a(){
 	var lines []string // input
 	//var p []map[string]map[string]int // holds maps with key:value pairs strings:ints
@@ -79,3 +205,4 @@ func d13a(){
 func main() {
 	d13a()
 }
+*/
