7d05b679c2149cdb704222b3e1c809bd73870482
MJ <michaeljefferies08@gmail.com>
2018-07-12 13:37:17 +0100

    added d8.txt to 2015 directory

---
+++ 2015/d8.go
@@ -0,0 +1,30 @@
+package main;
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+	"strings"
+)
+
+func d8i()(){
+	f,err := os.Open("d8.txt")
+	if err != nil{
+		fmt.Println(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan(){
+		lines = append(lines,scanner.Text())
+	}
+	for i := range lines{
+		n := strings.Split(lines[i]," ")
+		fmt.Println(n)
+	}
+}
+
+func main(){
+	d8i()
+}
