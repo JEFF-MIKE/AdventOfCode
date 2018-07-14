63567e797e93ba68a2ab18361451cc624dc14bfa
MJ <michaeljefferies08@gmail.com>
2018-07-14 22:05:11 +0100

    done part1 of day 8

--- 2015/d8.go
+++ 2015/d8.go
@@ -8,6 +8,7 @@ import (
 )
 
 func d8i()(){
+	totalCount := 0
 	f,err := os.Open("d8.txt")
 	if err != nil{
 		fmt.Println(err)
@@ -22,7 +23,32 @@ func d8i()(){
 	for i := range lines{
 		n := strings.Split(lines[i],"")
 		fmt.Println(n)
+		stringCount := 0
+		charCount := 0
+		x := 0
+		for (x < (len(n)-1)) {
+			if n[x] != "\\" {
+				charCount += 1
+				stringCount += 1
+				x++
+				continue
+			} else {
+				if (n[x+1] == "\\") || (n[x+1] == "\""){
+					stringCount+=2 // count backslash
+					charCount++
+					x+=2
+					continue
+				} else if n[x+1] == "x"{
+					stringCount+=4 // count backslash + following characters
+					charCount+=1 // hex counts as single character
+					x+=4 // jump 4 spaces			
+				}
+			}	
+		}
+		fmt.Println("CharCount:",charCount-1,"stringCount",stringCount+1)
+		totalCount += ((stringCount+1)-(charCount-1))
 	}
+	fmt.Println(totalCount)
 }
 
 func main(){
