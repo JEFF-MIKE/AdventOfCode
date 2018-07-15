d39b87caa45b6e45b6984f6c42c23bce1d41ed1c
MJ <michaeljefferies08@gmail.com>
2018-07-15 23:53:43 +0100

    Day 8 part 2 completed

--- 2015/d8.go
+++ 2015/d8.go
@@ -51,6 +51,51 @@ func d8i()(){
 	fmt.Println(totalCount)
 }
 
+func d8ii()(){
+	totalCount := 0
+	f,err := os.Open("d8.txt")
+	if err != nil{
+		fmt.Println(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan(){
+		lines = append(lines,scanner.Text())
+	}
+	for i := range lines{
+		n := strings.Split(lines[i],"")
+		fmt.Println(n)
+		stringCount := 2
+		charCount := 0
+		x := 0
+		for (x < (len(n)-1)) {
+			if n[x] != "\\" {
+				charCount += 1
+				stringCount += 1
+				x++
+				continue
+			} else {
+				if (n[x+1] == "\\") || (n[x+1] == "\""){
+					stringCount+=3 // count backslash
+					charCount++
+					x+=2
+					continue
+				} else if n[x+1] == "x"{
+					stringCount+=4 // count backslash + following characters
+					charCount+=1 // hex counts as single character
+					x+=4 // jump 4 spaces			
+				}
+			}	
+		}
+		fmt.Println("CharCount:",charCount-1,"stringCount",stringCount+1)
+		totalCount += ((stringCount+1)-(charCount-1))
+	}
+	fmt.Println(totalCount)
+}
+
 func main(){
 	d8i()
+	d8ii()
 }
